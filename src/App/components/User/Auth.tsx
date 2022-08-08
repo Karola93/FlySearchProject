76a87ff2ce0fac95947ff3477b5a15fbee29a05c
Karolina Dudek <karolinadudek9@gmail.com>
2022-08-08 20:09:18 +0200

    feat(Auth) : add email and password state

--- src/App/components/User/Auth.tsx
+++ src/App/components/User/Auth.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Button from 'App/components/Reuse/Button';
 import Input from 'App/components/Reuse/Input';
@@ -9,25 +9,38 @@ import { authActions } from 'App/store/auth';
 
 function Auth() {
   const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const loginHandler = (event) => {
-    event.preventDefault();
+  const loginHandler = (e) => {
+    e.preventDefault();
     dispatch(authActions.login());
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
   };
 
   return (
     <main className={classes.auth}>
-      <section>
-        <form onSubmit={loginHandler}>
-          <div className={classes.control}>
-            <Input type="email" id="email" />
-          </div>
-          <div className={classes.control}>
-            <Input type="password" id="password" min={8} />
-          </div>
-          <Button className={classes.btn} text="Login" type="submit" />
-        </form>
-      </section>
+      <form onSubmit={loginHandler}>
+        <div className={classes.control}>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label>
+            Email: <br />
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+        </div>
+        <div className={classes.control}>
+          <Input type="password" value={password} id="password" min={8} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <Button className={classes.btn} text="Login" type="submit" />
+      </form>
     </main>
   );
 }
