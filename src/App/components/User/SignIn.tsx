bcefab7ac410ae852340d2b23faaa245359a1c0d
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-08-16 18:57:48 +0200

    feat(SignIn): add hook useCookie

--- src/App/components/User/SignIn.tsx
+++ src/App/components/User/SignIn.tsx
@@ -8,6 +8,7 @@ import Button from 'App/components/Reuse/Button';
 import { useNavigate } from 'react-router-dom';
 import { authActions } from 'App/store/auth';
 import { useDispatch } from 'react-redux';
+import { useCookies } from 'react-cookie';
 
 function SignIn() {
   const dispatch = useDispatch();
@@ -16,12 +17,14 @@ function SignIn() {
     email: '',
     password: ''
   });
+  const [cookies, setCookies] = useCookies(['']);
 
   function signinHandler(e) {
     e.preventDefault();
     dispatch(authActions.login());
     navigate('/');
     localStorage.setItem('newUser', JSON.stringify(newUser));
+    setCookies('newUser', newUser);
   }
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </Provider>
   </BrowserRouter>
 );
