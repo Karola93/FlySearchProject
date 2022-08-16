2a3a8bc3d6695c840b5296148a72d2ce6357452e
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-08-16 18:30:05 +0200

    feat(index.tsx): install react-cookie

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// eslint-disable-next-line import/extensions
 import store from 'App/store';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-// eslint-disable-next-line import/order
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
