cf5a792b54fe361b3c812449d65ca69b929eb101
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-08-18 20:53:57 +0200

    style(SearchBox.module.css) change style

--- src/App/components/SearchBox/SearchBox.tsx
+++ src/App/components/SearchBox/SearchBox.tsx
@@ -6,7 +6,7 @@ import Button from 'App/components/Reuse/Button';
 function SearchBox() {
   return (
     <form className={classes.search}>
-      <div>
+      <div className={classes.margin}>
         <label>
           <input type="radio" /> One way
         </label>
@@ -14,13 +14,13 @@ function SearchBox() {
           <input type="radio" /> Roundtrip
         </label>
       </div>
-      <div>
+      <div className={classes.margin}>
         <label htmlFor="from">From </label>
         <input type="text" id="from" />
         <label htmlFor="to"> To </label>
         <input type="text" id="depart" />
       </div>
-      <div>
+      <div className={classes.margin}>
         <div>
           <label htmlFor="depart">Depart </label>
           <input type="text" id="to" />
@@ -29,9 +29,8 @@ function SearchBox() {
           <label htmlFor="travelers"> Travelers </label>
           <input type="number" id="travelers" min="1" style={{ width: 35 }} />
         </div>
-        <div></div>
       </div>
-      <div>
+      <div style={{ alignSelf: 'flex-end' }} className={classes.margin}>
         <Button type="submit" text="Search" />
       </div>
     </form>
