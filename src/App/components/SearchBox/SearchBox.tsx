5db843ac93321365901fc7d514b52c0680d00de0
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-08-18 12:44:05 +0200

    feat(SearchBox) add new div with Departure and return data input

--- src/App/components/SearchBox/SearchBox.tsx
+++ src/App/components/SearchBox/SearchBox.tsx
@@ -15,13 +15,18 @@ function SearchBox() {
         </label>
       </div>
       <div>
-        <label htmlFor="from">From</label>
+        <label htmlFor="from">From </label>
         <input type="text" id="from" />
-        <label htmlFor="to">To</label>
-        <input type="text" id="to" />
+        <label htmlFor="to"> To </label>
+        <input type="text" id="depart" />
       </div>
       <div>
-        <div></div>
+        <div>
+          <label htmlFor="depart">Depart </label>
+          <input type="text" id="to" />
+          <label htmlFor="return"> Return </label>
+          <input type="text" id="return" />
+        </div>
         <div></div>
       </div>
       <div>
