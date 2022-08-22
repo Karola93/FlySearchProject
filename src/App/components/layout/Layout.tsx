2c6f662532defac96ce396c4dd82b203958e69b6
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-08-22 18:30:29 +0200

    refactor(store): remove unnecessary module

--- src/App/components/layout/Layout.tsx
+++ src/App/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import classes from 'App/components/layout/Layout.module.scss';
 import MainNavigation from './MainNavigation';
+import classes from 'App/components/layout/Layout.module.scss';
 
 function Layout({ children }) {
   return (
